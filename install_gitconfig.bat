c2409a9a5f3dc8cd8b20e48e872ddebbed517e43
Martin Kovarik <kovarik.martin@gmail.com>
2011-08-14 22:33:36 +0200

    diff tool for git is created by bash script

--- install_gitconfig.bat
+++ install_gitconfig.bat
@@ -5,8 +5,10 @@ set TARGETDIR=%USERPROFILE%
 
 echo Deleting old symbolic links...
 del "%TARGETDIR%\.gitconfig"
+del "%TARGETDIR%\difftool.sh"
 echo Done
 
 echo Creating new symbolic links...
 mklink "%TARGETDIR%\.gitconfig"  "%CWD%\.gitconfig"
+mklink "%TARGETDIR%\difftool.sh"  "%CWD%\difftool.sh"
 echo Done
